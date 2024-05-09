2d76afa1c578dea7cc11431f86030ac32c056179
dokkaebi611 <dlee9@csuchico.edu>
2024-05-08 18:52:22 -0700

    Level2

--- SpyMerge/Assets/Scripts/ArrowController.cs
+++ SpyMerge/Assets/Scripts/ArrowController.cs
@@ -4,41 +4,59 @@ using UnityEngine;
 
 public class ArrowSignController : MonoBehaviour
 {
-    public GameObject arrowSignPrefab; // 화살표 표시 프리팹
-    private Transform player; // 플레이어의 Transform
+    public GameObject arrowSignPrefab;
+    private GameObject arrowSignInstance;
 
-    public float activationDistance = 5f; // 화살표가 활성화되는 거리
+    public float activationDistance = 5f;
+    private Transform player;
 
-    private GameObject arrowSignInstance; // 생성된 화살표 표시 오브젝트의 참조
+    public enum Direction
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest
+    }
 
+    [Header("Manual Direction Setting")]
+    public Direction arrowDirection;
 
     private void Start()
     {
         player = GameObject.Find("Player").transform;
     }
+
     void Update()
     {
-        // 플레이어와 화살표 사이의 거리 계산
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        // 플레이어가 일정 거리 내에 있으면 화살표를 생성하고, 그렇지 않으면 파괴
         if (distanceToPlayer <= activationDistance)
         {
             if (arrowSignInstance == null)
             {
-                // 플레이어가 일정 거리 내에 있고, 화살표가 생성되지 않은 경우
-                // 화살표를 생성합니다.
                 arrowSignInstance = Instantiate(arrowSignPrefab, transform.position, Quaternion.identity);
+                SetArrowDirection(); // Set direction when instantiated
             }
         }
         else
         {
             if (arrowSignInstance != null)
             {
-                // 플레이어가 일정 거리 밖에 있을 때
-                // 화살표를 파괴합니다.
                 Destroy(arrowSignInstance);
             }
+        }
+    }
+
+    private void SetArrowDirection()
+    {
+        if (arrowSignInstance != null)
+        {
+            float targetAngle = ((int)arrowDirection) * 45;
+            arrowSignInstance.transform.rotation = Quaternion.Euler(0, targetAngle, 0);
         }
     }
 }
